541d51758c5881657bbb9687e787d141c747ea95
Vivian Ho <hovivian.wy@gmail.com>
2022-09-29 21:02:25 +0800

    updated code

--- main.js
+++ main.js
@@ -1,36 +1,45 @@
 // Cards Related
 const $container = $(".container");
 const $card = $(".card");
-let choices = []
+const card = document.querySelector(".card");
+let choices = [];
+const matched = [];
 
 // Controls
-const $startGameBtn = $("#start-btn");
+const $startGameBtn16 = $("#start-btn-16");
+const $startGameBtn36 = $("#start-btn-36");
+const $resetBtn = $("#reset")
+const $startPage = $(".startPage");
+const $clickSound = $("#clickSound")
+
+// Game finishes
+const $endPageContainer = $(".endPageContainer")
+const $results = $(".results");
 
 // Points
 const $displayPoints = $("#point");
 let $points = 0;
 
-// timer
+// Timer
 const $timeLeft = $("#timeLeft");
 const $startingMinute = 1;
-let timeInterval = null
+let timeInterval = null;
 let $time = $startingMinute * 60;
 
 const updateTime = () => {
-  if ($time >= 0) {
+  if ($time>=0) {
     const $minutes = Math.floor($time / 60);
     let $seconds = $time % 60;
     $seconds = $seconds < 10 ? '0' + $seconds : $seconds;
     $timeLeft.text(`${$minutes}: ${$seconds}`);
     $time--;
-  } else {
-    clearInterval(timeInterval)
-  }
+  } else if ($time<0){
+    clearInterval(timeInterval);
+    gamePlayLoses();
+  } else if ($points=80){
+    clearInterval(timeInterval);
+    gamePlayWins();
 }
-
-const flipCard = (e) => {
-  const $flip = $(e.currentTarget);
-  $flip.toggleClass('flip');
 }
 
 const addPoints = () => {
@@ -44,6 +53,7 @@ const matchedCards = () => {
   if (choices.length === 2) {
     if (choices[0].attr("data-type") === choices[1].attr("data-type")) {
       addPoints();
+      matched.push(choiceOne)
       console.log("matched cards!");
     } else {
       console.log("the cards don't match!");
@@ -62,38 +72,112 @@ const matchedCards = () => {
   }
 }
 
+const clickSound = () => {
+  $clickSound[0].play();
+}
+
+const wrongSound = () => {
+  $wrongSound[0].play();
+}
+
+const correctSound = () => {
+  $correctSound[0].play();
+}
+
+const flipCard = (e) => {
+  const $flip = $(e.currentTarget);
+  $flip.toggleClass('flip');
+}
+
 const handleClick = (e) => {
-  const $clicked = $(e.currentTarget);
-  const id = $clicked.attr('data-id')
-  const alreadyExist = choices.find(($elem) => $elem.attr('data-id') === id)
-  if (!alreadyExist) {
-    flipCard(e);
-    choices.push($clicked);
-    matchedCards();
-  }
+    const $clicked = $(e.currentTarget);
+    const id = $clicked.attr('data-id');
+    const alreadyExist = choices.find(($elem) => $elem.attr('data-id') === id);
+    if (!alreadyExist) {
+      flipCard(e);
+      clickSound();
+      choices.push($clicked);
+      matchedCards();
+    }
 }
 
-const startGame = () => {
-  // get value of the selected difficulty
-  timeInterval = setInterval(updateTime, 1000);
-  generateGrid(4)
-  //hide start game button
-}
-
-$card.on("click", handleClick);
-$startGameBtn.on("click", startGame);
-
-
-// const gamePlay = () => {
-//   if (cardNum === 0) {
+// const gamePlay16 = () => {
+//   console.log("hahaha");
+//   if (matched.length ===8){
+//     console.log("game end!");
+//     $endPageContainer.removeClass("hide");
 //     $points += ($time * 10);
-//     popup message: `congrats! you scored ${$points}`
-//     show play again button
-//   } else if ($timeLeft === 0) {
-//     popup message: `Time's up! you scored ${$points}`
-//     show play again button
+//     $results.html(`<h2>Congratulations! You scored ${$points}!</h2>`);
+//     // $gameFinished = true;
+//   }  else if ($points === 80) {
+//     console.log("game end!");
+//     $endPageContainer.removeClass('hide');
+//     $results.html(`<h2>Time's up! You scored ${$points}!</h2>`);
+//     // $gameFinished = true;
 //   }
 // }
+
+const gamePlayWins = () => {
+    if(matched.length===8)
+    console.log("game end!");
+    $endPageContainer.removeClass("hide");
+    // $points += ($time * 10);
+    $results.html( `
+      <div>
+        <h2>Congratulations! You scored ${$points += ($time * 10)} points!</h2>
+        <p> Here is your fortune of the day: ${matched[0]}</p>
+        <p>Start another game?</p>
+      </div>
+    `)
+}
+
+const gamePlayLoses = () => {
+    $endPageContainer.removeClass('hide');
+    $results.html( `
+      <div>
+        <h2>Time's up! You scored ${$points} points!</h2>
+        <p> Here is your fortune of the day: ${matched[0]}</p>
+        <p>Start another game?</p>
+      </div>
+    `)
+}
+
+// const gamePlay36 = () => {
+//   if ($points===160) {
+//     $endPageContainer.removeClass('hide')
+//     $points += ($time * 10);
+//     $results.html(`<h2>Congratulations! You scored ${$points}!</h2>`);
+//   } else if($timeLeft === 0){
+//     $endPageContainer.removeClass(hide)
+//     $results.html(`<h2>Time's up! You scored ${$points}!</h2>`);
+//     return gameFinished = true;
+//   }
+// }
+
+const startGame16 = () => {
+  timeInterval = setInterval(updateTime, 1000);
+  generateGrid(4);
+  $startPage.remove();
+  $endPageContainer.addClass('hide');
+}
+
+const startGame36 = () => {
+  timeInterval = setInterval(updateTime, 1000);
+  generateGrid(6);
+  $startPage.remove();
+  $endPageContainer.addClass('hide');
+}
+
+const reset16 = () => {
+
+}
+
+$startGameBtn16.on("click", startGame16);
+$startGameBtn36.on("click", startGame36);
+
+$resetBtn.on("click", function () {
+  location.reload(true);
+});
 
 const shuffle = (array) => {
   let currentIndex = array.length,
@@ -115,21 +199,23 @@ const shuffle = (array) => {
   return array;
 }
 
-// num is 4, 6, 8...
 const generateGrid = (rows) => {
-  const baseImageKeys = Array((rows * rows) / 2).fill().map((_, i) => `${i + 1}`)
-  const allImageKeys = [...baseImageKeys, ...baseImageKeys]
-  const shuffled = shuffle(allImageKeys)
-  $container.addClass(`rows-col-${rows}`)
+  const baseImageKeys = Array((rows * rows) / 2).fill().map((_, i) => `${i + 1}`);
+  const allImageKeys = [...baseImageKeys, ...baseImageKeys];
+  const shuffled = shuffle(allImageKeys);
   shuffled.forEach((imageKey, i) => {
     $container.append(`
-      <div class="card" data-id = "${i}" data-type="card-${imageKey}" >
+      <div class="card rows-col-${rows * rows}" data-id="${i}" data-type="card-${imageKey}" id="gameCards">
         <img src="game-images/front-view-${imageKey}.png" alt="" class="front-view view"/>
         <img src="game-images/back-view.png" alt="" class="back-view view" />
       </div>
     `)
-  })
+  });
 }
+
+$container.on("click", '.card', handleClick);
+
+
 
 //bonus
 // const $firstMove = $();
@@ -138,4 +224,4 @@ const generateGrid = (rows) => {
 // const cardReading: {
 //   card1: "You'll feel drawn to the comforts of your bed this afternoon.",
 //   card2: "Your aura will be blessed with a powerful and mysterious allure this morning."
-// }
+//
